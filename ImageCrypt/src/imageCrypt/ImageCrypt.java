476e98f865415db60d40f7708da0c0137b050960
Jacob Stilwell <jstilwell2011@hotmail.com>
2020-03-04 16:01:23 -0600

    main method and imagecrypt encryption implemented

--- ImageCrypt/src/imageCrypt/ImageCrypt.java
+++ ImageCrypt/src/imageCrypt/ImageCrypt.java
@@ -1,5 +1,125 @@
 package imageCrypt;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jacob Stilwell
+ */
 public class ImageCrypt {
 
+    private String message;
+    private BufferedImage basicImage;
+    private BufferedImage encryptedImage;
+    private final int MAX = 255;
+    private final int MIN = 0;
+
+    public ImageCrypt() {
+        this.message = null;
+        this.basicImage = null;
+    }
+
+    public ImageCrypt(String message, BufferedImage basicImage) {
+        this.message = message;
+        this.basicImage = basicImage;
+        this.encryptedImage= basicImage;
+    }
+    public ImageCrypt(BufferedImage encryptedImage, BufferedImage basicImage) {
+        this.encryptedImage = encryptedImage;
+        this.basicImage = basicImage;
+        this.message=null;
+    }
+    public void encrypt() {
+        String binary = getBinary();
+        int binLocation = 0;
+        boolean finished=false;
+        System.out.println(basicImage.getHeight()+" "+basicImage.getWidth());
+        for (int i = 0; i < basicImage.getHeight() && !finished; i++) {
+            for (int j = 0; j < basicImage.getWidth(); j++) {
+                Color pixel = new Color(basicImage.getRGB(i, j));
+                if (binary.charAt(binLocation) == '1') {
+                    if (pixel.getBlue() < MAX) {
+                        pixel = new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue() + 1);
+                    } else {
+                        pixel = new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue() - 1);
+                    }
+                    //System.out.println(i+" "+j);
+                    encryptedImage.setRGB(i, j, pixel.getRGB());
+                }
+                binLocation++;
+                if(binLocation==binary.length()-1){
+                    finished=true;
+                    break;
+                }
+            }
+        }
+        //return encryptedImage;
+    }
+
+    private String decrypt() {//TODO
+        StringBuilder binary = new StringBuilder();
+        boolean finished=false;
+        for (int i = 0; i < encryptedImage.getHeight(); i++) {
+            for (int j = 0; j < encryptedImage.getWidth(); j++) {
+                Color encryptedPixel = new Color(encryptedImage.getRGB(i, j));
+                Color basicPixel = new Color(basicImage.getRGB(i, j));
+                if(encryptedPixel.getBlue()!=basicPixel.getBlue())binary.append('1');
+                else binary.append('0');                 
+            }
+        }
+        return binary.toString();
+    }
+
+    private String getBinary() {
+        byte[] bytes = message.getBytes();
+        StringBuilder binary = new StringBuilder();
+        for (byte b : bytes) {
+            int val = b;
+            for (int i = 0; i < 8; i++) {
+                binary.append((val & 128) == 0 ? 0 : 1);
+                val <<= 1;
+            }
+        }
+        return binary.toString();
+    }
+
+    private String getString() {//TODO
+        byte[] bytes = message.getBytes();
+        StringBuilder binary = new StringBuilder();
+        for (byte b : bytes) {
+            int val = b;
+            for (int i = 0; i < 8; i++) {
+                binary.append((val & 128) == 0 ? 0 : 1);
+                val <<= 1;
+            }
+        }
+        return binary.toString();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public BufferedImage getBasicImage() {
+        return basicImage;
+    }
+
+    public void setBasicImage(BufferedImage basicImage) {
+        this.basicImage = basicImage;
+    }
+
+    public BufferedImage getEncryptedImage() {
+        return encryptedImage;
+    }
+
+    public void setEncryptedImage(BufferedImage encryptedImage) {
+        this.encryptedImage = encryptedImage;
+    }
 }
--- ImageCrypt/src/imageCrypt/Main.java
+++ ImageCrypt/src/imageCrypt/Main.java
@@ -1,10 +1,54 @@
 package imageCrypt;
 
+
+//import com.mycompany.imagecrypt.ImageCrypt;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import javax.imageio.ImageIO;
+
+/**
+*
+* @author jstil
+*/
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+      BufferedImage image= null;
+      String message="";
+      ImageCrypt imageCrypt;
+      try{
+          File imageFile = new File("G://flag.png");//picture source
+          image = ImageIO.read(imageFile);
+      }catch(IOException e){
+          e.printStackTrace();
+      }
+      try{ 
+          File textFile = new File("G://message.txt");//text source
+          Scanner scanner = new Scanner(textFile);
+          while(scanner.hasNext()){
+              message+=" "+scanner.next();
+          }
+          scanner.close(); 
+          System.out.println(message);
+      }catch(InputMismatchException i){
+          i.printStackTrace();
+      }catch(FileNotFoundException f){
+          f.printStackTrace();
+      }
+      imageCrypt = new ImageCrypt(message, image);
+      //try{
+          File output = new File("G://encryptedFlag.png");
+//          imageCrypt.setEncryptedImage(imageCrypt.encrypt());
+//          ImageIO.write(imageCrypt.encrypt(), "png", output);
+      //}catch(IOException e){
+         // e.printStackTrace();
+      //}
+  }
+  
 }
